620801b6f50c08172633bd580ad8510477a9662b
niubajr19 <lucas.antonio199@outlook.com>
2020-12-14 16:31:03 -0300

    Criação da table Users

---
+++ server/src/database/migrations/1607973946388-CreateUsers.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1607973946388 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          // Quando o usuário foi criado
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          // Quando foi atualizado
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
