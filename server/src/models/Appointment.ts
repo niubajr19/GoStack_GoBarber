267b67f2a7cecac9638bf402bb84ecaa03814e67
niubajr19 <lucas.antonio199@outlook.com>
2020-12-14 16:22:19 -0300

    Backend enviando dados para o BD - Postgres

---
+++ server/src/models/Appointment.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+// Sempre que atualizado, será salvo na tabela 'appointments'
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  // Criado de forma automática no TypeOrm
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
+}
+
+export default Appointment;
